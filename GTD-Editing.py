d25811c71f4fb2c0f6d3e536d4ae248056846eff
Arghasree Banerjee <banerjeearghasree@gmail.com>
2019-06-29 10:35:20 +0530

    Update GTD-Editing.py
    Imported the required libraries

--- GTD-Editing.py
+++ GTD-Editing.py
@@ -1,5 +1,11 @@
+#Importing Libraries
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 #importing the raw Global Terrorism Database and storing it in datsaset
 dataset = pd.read_csv("/Users/kushankurghosh/Documents/ML(IT)- Files/Project/Global Terrorism Database.csv")
 dataset.isnull().sum()
